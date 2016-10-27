0947a7009fbc2f59ec03ae0b34542cd6f96d254e
nielsv <niels.vandekeybus@tenforce.com>
2016-10-27 11:54:20 +0200

    corrected script to run any spec file

--- build-rpms.sh
+++ build-rpms.sh
@@ -4,4 +4,4 @@
 su rpmbuild -l -c "mkdir -p /home/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS} && echo '%_topdir /home/rpmbuild' > /home/rpmbuild/.rpmmacros"
 
 # parse arguments
-su rpmbuild -l -c "pushd /home/rpmbuild/SPECS && rpmbuild -ba lodms.spec && popd"
+su rpmbuild -l -c "pushd /home/rpmbuild/SPECS && rpmbuild -ba *.spec && popd"
